76749250efdca4e5c91bc3556cd2aa34f95166ab
KylderiNascimento <kylderi.nascimento@hotmail.com>
2022-07-30 05:46:09 -0300

    swagger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
-
+//rotas da documentação
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+//rotas da aplicação
 app.use("/users", usersRoutes);
 
 export { app };
